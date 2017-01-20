87d6939c7e09d6bb49c32175c3f2d1d3b5e25ca9
mihar-22 <rahim_alwer@hotmail.com>
2017-01-20 12:12:19 +1100

    refactor + add request collection to session manager

--- Mylogbook/Session.swift
+++ Mylogbook/Session.swift
@@ -1,15 +1,17 @@
 
 import Alamofire
+import ObjectMapper
+import AlamofireObjectMapper
 
 typealias Message = String
 typealias Data = [String: Any]
 typealias Error = [String: String]
 
-// MARK: ApiResponse
+// MARK: API Response
 
-struct ApiResponse {
+struct ApiResponse<Data> {
     let statusCode: Int
-    let message: Message
+    let message: Message?
     let data: Data?
     let errors: Error?
 }
@@ -27,21 +29,45 @@ class Session {
         manager.adapter = ApiTokenAdapter()
     }
     
-    func requestJSON(_ route: Routable, completion: @escaping (ApiResponse) -> Void) {
+    func requestJSON(_ route: Routable, completion: @escaping (ApiResponse<Data>) -> Void) {
+        
         manager.request(route).responseJSON { response in
-            guard let json = response.result.value as? [String: Any] else { return }
+            guard let apiResponse = self.unpackJSONResponse(response) else { return }
             
-            guard let statusCode = response.response?.statusCode else { return }
-            
-            let message = json["message"] as! Message
-            
-            let data = json["data"] as? Data
-            
-            let errors = json["errors"] as? Error
-            
-            let _response = ApiResponse(statusCode: statusCode, message: message, data: data, errors: errors)
-            
-            completion(_response)
+            completion(apiResponse)
         }
+        
+    }
+    
+    func requestCollection<Model: Mappable>(_ route: Routable, completion: @escaping (ApiResponse<[Model]>) -> Void) {
+        
+        manager.request(route).responseArray(keyPath: "data") { (response: DataResponse<[Model]>) in
+            guard let apiResponse: ApiResponse<[Model]> = self.unpackArrayResponse(response) else { return }
+
+            completion(apiResponse)
+        }
+        
+    }
+    
+    private func unpackJSONResponse(_ response: DataResponse<Any>) -> ApiResponse<Data>? {
+        guard let json = response.result.value as? [String: Any] else { return nil }
+        
+        guard let statusCode = response.response?.statusCode else { return nil }
+        
+        let message = json["message"] as? Message
+        
+        let data = json["data"] as? Data
+        
+        let errors = json["errors"] as? Error
+        
+        return ApiResponse<Data>(statusCode: statusCode, message: message, data: data, errors: errors)
+    }
+    
+    private func unpackArrayResponse<Model: Mappable>(_ response: DataResponse<[Model]>) -> ApiResponse<[Model]>? {
+        guard let statusCode = response.response?.statusCode else { return nil }
+        
+        let collection = response.result.value
+        
+        return ApiResponse<[Model]>(statusCode: statusCode, message: nil, data: collection, errors: nil)
     }
 }
