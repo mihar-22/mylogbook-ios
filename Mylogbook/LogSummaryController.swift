bbc72edcc4f3dedfa9abf62ce31474f8af636073
mihar-22 <rahim_alwer@hotmail.com>
2017-02-22 12:43:32 +1100

    add driven distance to saved odometer at the end of a trip

--- Mylogbook/LogSummaryController.swift
+++ Mylogbook/LogSummaryController.swift
@@ -10,8 +10,22 @@ class LogSummaryController: UIViewController {
     @IBAction func didTapSave(_ sender: UIBarButtonItem) {
         TripStore.add(trip)
         
+        saveOdometer()
+        
         tabBarController!.selectedIndex = 0
         
         navigationController!.popToRootViewController(animated: true)
+    }
+    
+    // MARK: Odometer
+    
+    func saveOdometer() {
+        let key = "car-\(trip.car!.id)-odometer"
+        
+        let odometer = Int(UserDefaults.standard.string(forKey: key)!)!
+        
+        let distance = Int(round(trip.distance))
+        
+        UserDefaults.standard.set((odometer + distance), forKey: key)
     }
 }
