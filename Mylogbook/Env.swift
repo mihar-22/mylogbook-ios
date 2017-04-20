730de2601cca5d969813bf6fcd32f04bdf2e5be8
mihar-22 <rahim_alwer@hotmail.com>
2017-04-20 13:57:50 +1000

    set production domain

--- Mylogbook/Env.swift
+++ Mylogbook/Env.swift
@@ -5,6 +5,6 @@ struct Env {
 #if DEBUG
     static let MLB_API_BASE = "http://mylogbook.dev/api/v1"
 #else
-    static let MLB_API_BASE = "https://mylogbook.com/api/v1"
+    static let MLB_API_BASE = "https://mylb.com.au/api/v1"
 #endif
 }
